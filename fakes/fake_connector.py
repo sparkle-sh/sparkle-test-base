ca2a2eef758ddcb962414c3414e5a7e756e709f1
nekoffski <mrnek135@gmail.com>
2020-07-15 20:01:15 +0200

    fixed fake connector

--- fakes/fake_connector.py
+++ fakes/fake_connector.py
@@ -11,24 +11,34 @@ class FakeConnector(threading.Thread):
     def __init__(self):
         super().__init__()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.is_running = True
         self.queue = queue.Queue()
+        self.client = None
 
     def run(self):
-        self.socket.bind(("127.0.0.1", CONNECTOR_PORT))
-        self.socket.listen(1)
-        conn, addr = self.socket.accept()
-        while self.is_running:
-            conn.recv(1024)
-            payload = json.dumps(self.queue.get())
-            conn.sendall(struct.pack("I", len(payload)))
-            conn.sendall(payload.encode())
+        try:
+            self.socket.bind(("127.0.0.1", CONNECTOR_PORT))
+            self.socket.listen(5)
+
+            self.client, addr = self.socket.accept()
+            while self.is_running:
+                self.client.recv(1024)
+                payload = json.dumps(self.queue.get())
+                self.client.sendall(struct.pack("I", len(payload)))
+                self.client.sendall(payload.encode())
+        except OSError:
+            if self.is_running:
+                raise
 
     def enqueue_response(self, response):
         self.queue.put(response)
 
     def stop(self):
         if self.is_running:
-            self.is_running = False
             self.socket.close()
+            self.is_running = False
+            if self.client:
+                self.client.shutdown(socket.SHUT_RDWR)
+                self.client.close()
             self.join()
