c30d842e03132c6e953c81664e67e43c1e95580b
nekoffski <mrnek135@gmail.com>
2020-07-15 22:30:35 +0200

    Added datasheets

--- models/device.py
+++ models/device.py
@@ -7,15 +7,42 @@ class DeviceType(enum.Enum):
     SENSOR = 2
 
 
+class Datasheet(object):
+    def serialize(self) -> dict:
+        raise NotImplementedError
+
+@dataclasses.dataclass
+class SwitchableDeviceDatasheet(Datasheet):
+    states: []
+
+    def serialize(self) -> dict:
+        return {
+            "states": self.states        
+        }
+
+
+@dataclasses.dataclass
+class SensorDeviceDatasheet(Datasheet):
+    labels: []
+
+    def serialize(self) -> dict:
+        return {
+            "labels": self.labels        
+        }
+
+
 @dataclasses.dataclass
 class Device(object):
     dtype: DeviceType = DeviceType.SWITCHABLE
     name: str = 'example_name'
     description: str = 'example_description'
+    datasheet: Datasheet = SwitchableDeviceDatasheet
 
     def serialize(self) -> dict:
         return {
             "type": self.dtype.value,
             "name": self.name,
-            "description": self.description
+            "description": self.description,
+            "datasheet":  self.datasheet.serialize()
         }
+
